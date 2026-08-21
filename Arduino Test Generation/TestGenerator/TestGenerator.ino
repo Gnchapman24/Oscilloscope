3b1a0ec3ffeac0e96f1685a7b4efa97e091a8270
gnchapman24 <gnchapman24@gmail.com>
2026-08-21 11:03:30 -0400

    Initial signal gen test: need capacitor

--- Arduino Test Generation/TestGenerator/TestGenerator.ino
+++ Arduino Test Generation/TestGenerator/TestGenerator.ino
@@ -1,11 +1,25 @@
 
+// Constants
+const float MAX_VOLTAGE = 5.0;
+
+// Pin declarations
+int signalPin = 3;
+int analogPin = A5;
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Serial testing...");
+
+  pinMode(signalPin, OUTPUT);
+  analogWrite(signalPin, 0);
 }
 
 void loop() { 
-  // put your main code here, to run repeatedly:
-  Serial.println(millis());
-  delay(1000);
+  for (int i = 0; i < 256; i++) {
+    analogWrite(signalPin, i);
+    Serial.print("Supplied value: ");
+    Serial.print( (((float) i)/256) * MAX_VOLTAGE);
+
+    Serial.print(" | Read voltage: ");
+    Serial.println(analogRead(analogPin));
+  }
 }
